9156d33984a2dada9673f2f0c1405d0b7ad30d82
ytakemon <yuka.takemon@gmail.com>
2017-09-12 08:15:42 -0400

    Annotation of objects transferred. Additional to come from Gary

--- Data_transfer.sh
+++ Data_transfer.sh
@@ -1,5 +1,21 @@
 cd /projects/cgd/QTL_mapping/kidney2/
-cp DO188b_kidney* /projects/korstanje-lab/ytakemon/JAC_DO_Kidney/data
+cp DO188b_kidney* /projects/korstanje-lab/ytakemon/JAC_DO_Kidney/RNAseq_data
 
-cd  /hpcdata/gac/derived
-cp -r JAC_DO_Kidney_RNASeq/ /projects/korstanje-lab/ytakemon/JAC_DO_Kidney/data/derived
+# Annotation of data contained in RData:
+# DO188b_kidney.RData:
+## G - 188x188 matrix ?????? kinship?
+## Glist - G by chromosomes
+## N - list containing number of attributes
+## annot.mrna - df mrna annotation (gene id, chr, gene sybol, etc...)
+## annot.protein - df protein annoation
+## annot.samples - df sample annotation
+## covar - 1/0 covariate list
+## expr.mrna - mRNA counts are they? how have they been normalized?
+## expr.protein - same questions as above
+## genoprobs - Genotype probability of all 188 animals
+## raw.mrna - Raw mRNA reads (raw... counts?)
+## raw.protein - Raw protein
+## snps - 64K Marker id, chrom, posisiton
+
+cd /hpcdata/gac/projects/JAC_DO_Phenotypes
+cp -r * /projects/korstanje-lab/ytakemon/JAC_DO_Kidney/Phenotype/
