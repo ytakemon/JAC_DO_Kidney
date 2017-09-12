02a514f750fae9452872630ccec686f93ce071d3
ytakemon <yuka.takemon@gmail.com>
2017-09-12 07:10:09 -0400

    Initial commit: data transfer records

---
+++ Data_transfer.sh
@@ -0,0 +1,5 @@
+cd /projects/cgd/QTL_mapping/kidney2/
+cp DO188b_kidney* /projects/korstanje-lab/ytakemon/JAC_DO_Kidney/data
+
+cd  /hpcdata/gac/derived
+cp -r JAC_DO_Kidney_RNASeq/ /projects/korstanje-lab/ytakemon/JAC_DO_Kidney/data/derived
