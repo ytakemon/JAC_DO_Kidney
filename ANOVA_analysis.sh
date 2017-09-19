c1e886d50ea0e667ed2fe7064a177b5267983eed
ytakemon <yuka.takemon@gmail.com>
2017-09-19 12:14:02 -0400

    Rename

---
+++ ANOVA_analysis.sh
@@ -0,0 +1,9 @@
+# Open interactive session on Cadillac
+cd /projects/korstanje-lab/ytakemon/JAC_DO_Kidney
+
+# Load modules
+# module load R/3.3.2
+
+# ANOVA analysis using Petr's Rscirpt (take < 30mins)
+# https://github.com/simecek/TheAgingProteome/blob/master/code/anova_tests.R
+Rscript ./Scripts/anova_tests.R ./RNAseq_data/DO188b_kidney_noprobs.RData ./Anova_output/kidney_anova_output.csv
