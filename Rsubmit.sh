692d897adcdd155c01a7c811fffab038c6fd6c61
ytakemon <yuka.takemon@gmail.com>
2017-09-19 09:34:30 -0400

    style fix

--- Rsubmit.sh
+++ Rsubmit.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -l
 #PBS -l nodes=1:ppn=3,walltime=03:00:00
-#
+
 # Usage:
 # qsub -v script=${R script name without ".R"} Rsubmit.R
-#
+
 cd /projects/korstanje-lab/ytakemon/JAC_DO_Kidney/Scripts
 module load R/3.3.2
 
