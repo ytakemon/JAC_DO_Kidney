a555d172b013f428a8bbe5d8e284cf4e5b0691b3
ytakemon <yuka.takemon@gmail.com>
2017-09-19 15:11:29 -0400

    Finishing touches

--- ANOVA/ANOVA_analysis.sh
+++ ANOVA/ANOVA_analysis.sh
@@ -1,17 +1,16 @@
-# Go to Cadillac scripts directory
-cd /projects/korstanje-lab/ytakemon/JAC_DO_Kidney
+# Updating JAC DO Kidney ANOVA using Petr's scripts.
 
 # ANOVA analysis using Petr's Rscirpt (takes < 30mins)
 # https://github.com/simecek/TheAgingProteome/blob/master/code/anova_tests.R
+cd /projects/korstanje-lab/ytakemon/JAC_DO_Kidney
 # qsub -v I="input.Rdata output.csv",script=rscript_name
 qsub -v I="../RNAseq_data/DO188b_kidney_noprobs.RData ../Anova_output/kidney_anova_output.csv",script=anova_tests Rsubmit_args.R
 
 # Calculate fdr/pval-BH for every p-value calculated in Petr's code
 cd /projects/korstanje-lab/ytakemon/JAC_DO_Kidney/Scripts
+# qsub -v I="anova_output.csv output.csv",script=rscript_name
 qsub -v I="kidney_anova_output.csv kidney_anova_fdr_output.csv",script=Kidney_ANOVA_fdr Rsubmit_args.sh
 
-
-
-# Plot ANOVA output
+# Plot p-values and fdr from ANOVA output
 cd /projects/korstanje-lab/ytakemon/JAC_DO_Kidney/Scripts
-qsub -v I=kidney_anova_output.csv,script=Kidney_ANOVA_pval_hist Rsubmit_args.sh
+qsub -v I=kidney_anova_fdr_output.csv,script=Kidney_ANOVA_pval_hist Rsubmit_args.sh
--- ANOVA/Kidney_ANOVA_pval_hist.R
+++ ANOVA/Kidney_ANOVA_pval_hist.R
@@ -9,7 +9,7 @@
 
 # Usage on Cadillac
 # cd /projects/korstanje-lab/ytakemon/JAC_DO_Kidney/Scripts
-# qsub -v I=kidney_anova_output.csv,script=Kidney_ANOVA_pval_hist Rsubmit_args.R
+# qsub -v I=kidney_anova_fdr_output.csv,script=Kidney_ANOVA_pval_hist Rsubmit_args.R
 
 # R/3.3.2
 library(ggplot2)
@@ -21,50 +21,102 @@ args <- commandArgs(trailingOnly = TRUE)
 output <- list.files(pattern = paste0("^",args[[1]]), recursive = TRUE)
 output <- read.csv(output[[1]])
 
-# 1.1 mRNA - Age (adjusted for sex and generation)
-mRNA_age <- ggplot(output, aes(x=p.mRNA_Age.Sex)) +
+# Plot pvalues
+# 1.1.1 mRNA - Age (adjusted for sex and generation)
+pval_mRNA_age <- ggplot(output, aes(x=p.mRNA_Age.Sex)) +
     geom_histogram(binwidth=0.04) +
     theme_bw() +
     labs(title="Kidney",
          subtitle="mRNA/Age",
          x="p-value")
 # 1.2 protein - Age (adjusted for sex and generation)
-protein_age <- ggplot(output, aes(x=p.Prot_Age.Sex)) +
+pval_protein_age <- ggplot(output, aes(x=p.Prot_Age.Sex)) +
     geom_histogram(binwidth=0.04) +
     theme_bw() +
     labs(title="Kidney",
          subtitle="Protein/Age",
          x="p-value")
 # 2.1 mRNA - Sex (adjusted for age and gernation)
-mRNA_sex <- ggplot(output, aes(x=p.mRNA_Sex.Age)) +
+pval_mRNA_sex <- ggplot(output, aes(x=p.mRNA_Sex.Age)) +
     geom_histogram(binwidth=0.04) +
     theme_bw() +
     labs(title="Kidney",
          subtitle="mRNA/Sex",
          x="p-value")
 # 2.2 protein - Sex (adjusted for age and gernation)
-protein_sex <- ggplot(output, aes(x=p.Prot_Sex.Age)) +
+pval_protein_sex <- ggplot(output, aes(x=p.Prot_Sex.Age)) +
     geom_histogram(binwidth=0.04) +
     theme_bw() +
     labs(title="Kidney",
          subtitle="Protein/Sex",
          x="p-value")
 # 3.1 mRNA - Age:Sex interaction (age as variate)
-mRNA_int <- ggplot(output, aes(x=p.mRNA_Interaction)) +
+pval_mRNA_int <- ggplot(output, aes(x=p.mRNA_Interaction)) +
     geom_histogram(binwidth=0.04) +
     theme_bw() +
     labs(title="Kidney",
          subtitle="mRNA/(Age*Sex)",
          x="p-value")
 # 3.2 protein - Age:Sex interaction (age as variate)
-protein_int <- ggplot(output, aes(x=p.Prot_Interaction)) +
+pval_protein_int <- ggplot(output, aes(x=p.Prot_Interaction)) +
     geom_histogram(binwidth=0.04) +
     theme_bw() +
     labs(title="Kidney",
          subtitle="Protein/(Age*Sex)",
          x="p-value")
 
+# Plot fdr
+# 1.1 mRNA - Age (adjusted for sex and generation)
+fdr_mRNA_age <- ggplot(output, aes(x=fdr.mRNA_Age.Sex)) +
+    geom_histogram(binwidth=0.04) +
+    theme_bw() +
+    labs(title="Kidney",
+         subtitle="mRNA/Age",
+         x="fdr")
+# 1.2 protein - Age (adjusted for sex and generation)
+fdr_protein_age <- ggplot(output, aes(x=fdr.Prot_Age.Sex)) +
+    geom_histogram(binwidth=0.04) +
+    theme_bw() +
+    labs(title="Kidney",
+         subtitle="Protein/Age",
+         x="fdr")
+# 2.1 mRNA - Sex (adjusted for age and gernation)
+fdr_mRNA_sex <- ggplot(output, aes(x=fdr.mRNA_Sex.Age)) +
+    geom_histogram(binwidth=0.04) +
+    theme_bw() +
+    labs(title="Kidney",
+         subtitle="mRNA/Sex",
+         x="fdr")
+# 2.2 protein - Sex (adjusted for age and gernation)
+fdr_protein_sex <- ggplot(output, aes(x=fdr.Prot_Sex.Age)) +
+    geom_histogram(binwidth=0.04) +
+    theme_bw() +
+    labs(title="Kidney",
+         subtitle="Protein/Sex",
+         x="fdr")
+# 3.1 mRNA - Age:Sex interaction (age as variate)
+fdr_mRNA_int <- ggplot(output, aes(x=fdr.mRNA_Interaction)) +
+    geom_histogram(binwidth=0.04) +
+    theme_bw() +
+    labs(title="Kidney",
+         subtitle="mRNA/(Age*Sex)",
+         x="fdr")
+# 3.2 protein - Age:Sex interaction (age as variate)
+fdr_protein_int <- ggplot(output, aes(x=fdr.Prot_Interaction)) +
+    geom_histogram(binwidth=0.04) +
+    theme_bw() +
+    labs(title="Kidney",
+         subtitle="Protein/(Age*Sex)",
+         x="fdr")
 
-pdf("./Plot/pval-histograms.pdf", width = 12, height = 6)
-grid.arrange(mRNA_age, mRNA_sex, mRNA_int, protein_age, protein_sex, protein_int, ncol = 3)
+pdf("./Plot/mRNA-histograms.pdf", width = 12, height = 6)
+grid.arrange(pval_mRNA_age, pval_mRNA_sex, pval_mRNA_int,
+             fdr_mRNA_age, fdr_mRNA_sex, fdr_mRNA_int,
+             ncol = 3)
+dev.off()
+
+pdf("./Plot/protein-histograms.pdf", width = 12, height = 6)
+grid.arrange(pval_protein_age, pval_protein_sex, pval_protein_int, 
+             fdr_protein_age, fdr_protein_sex, fdr_protein_int, 
+             ncol = 3)
 dev.off()
