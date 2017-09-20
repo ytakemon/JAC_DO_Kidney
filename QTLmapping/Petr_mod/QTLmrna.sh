a464d020a3a33cf9862b2bbb2ca383676bd72f89
ytakemon <yuka.takemon@gmail.com>
2017-09-20 15:07:46 -0400

    Moved

--- QTLmapping/Petr_mod/QTLmrna.sh
+++
@@ -1,7 +0,0 @@
-#PBS -l nodes=1:ppn=1,walltime=22:00:00
-
-module load R
-
-cd ~/cgd/QTL_mapping/kidney2/scripts
-
-Rscript QTLmrna.r
--- QTLmapping/Petr_mod/scan_one_m.sh
+++
@@ -1,7 +0,0 @@
-#PBS -l nodes=1:ppn=10,walltime=12:00:00
-
-module load R
-
-cd ~/cgd/QTL_mapping/kidney2
-
-Rscript scripts/scan_one_m.r $col
