d9132e8dc775c5ad4c71eefb14fa1a7b17a644a8
ytakemon <yuka.takemon@gmail.com>
2017-09-22 14:51:06 -0400

    Fix typo

--- QTLmapping/Rsubmit_args.sh
+++ QTLmapping/Rsubmit_args.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -l
-#PBS -l nodes=1:ppn=3,walltime=03:00:00
+#PBS -l nodes=1:ppn=10,walltime=24:00:00
 
 # Usage:
-# qsub -v I=${from list},script=${R script name} Rsubmit_multiple.R
+# qsub -v I=${from list},script=${R script name} Rsubmit_args.sh
 
 cd /projects/korstanje-lab/ytakemon/JAC_DO_Kidney/Scripts/QTLmapping
-module load R/3.3.2
+module load R/3.4.1
 
 # I is the input variable
 R --no-save --args ${I} < ${script}.R > ${script}.Rout
