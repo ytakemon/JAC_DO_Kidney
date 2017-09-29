7460ceabab02c7cab70e8c1c0b2700a5a0ac49c6
ytakemon <yuka.takemon@gmail.com>
2017-09-29 10:15:14 -0400

    Adding interactive gene ID and symbol based on position

--- QTLmapping/eQTL_IntAge_map.R
+++ QTLmapping/eQTL_IntAge_map.R
@@ -1,12 +1,14 @@
 # R/3.4.1
 library(ggplot2)
+library(dplyr)
+library(biomaRt)
 setwd("/projects/korstanje-lab/ytakemon/JAC_DO_Kidney")
 eQTL_add <- read.csv("./QTLscan/output/eQTLAllAdditive.csv")
 eQTL_intAge <- read.csv("./QTLscan/output/eQTLAllInteractiveAge.csv")
 eQTL_intSex <- read.csv("./QTLscan/output/eQTLAllInteractiveSex.csv")
 eQTL_best <- read.csv("./QTLscan/output/eQTLBestperGene.csv")
 
-# Usinb eQTL_best to create plot
+# Using eQTL_best to create plot
 # need to reorder "chr" factors
 chr_full <- c("1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","X","Y", "MT")
 eQTL_best$chr <- factor(eQTL_best$chr, levels = chr_full)
@@ -30,6 +32,47 @@ LODthreshold_diff <- 7.5
 # Subset Int-Age LOD above total/full and diff
 Int_age <- eQTL_best[(eQTL_best$IntAgeLODDiff > LODthreshold_diff),] # above diff threshold
 Int_age <- Int_age[(Int_age$IntAgeLODFull > LODthreshold_total),] # above full/total threshold
+
+# Annotate Interactive-Age postion with genes and save file for sharing
+save_int_age <- Int_age[,c("id", "symbol","chr","start","end", "biotype", "IntAgeChr","IntAgePos","IntAgeLODDiff","IntAgeLODFull")]
+save_int_age <- arrange(save_int_age, IntAgeChr, IntAgePos)
+# Annotate
+ensembl <- useEnsembl(biomart = "ENSEMBL_MART_ENSEMBL",
+                      dataset = "mmusculus_gene_ensembl",
+                      verbose = TRUE)
+
+# use listFilters(ensembl) for full list of filters
+save_int_age$IntAgeGeneID <- 0
+save_int_age$IntAgeGeneSymbol <- 0
+for (i in 1:length(save_int_age$IntAgeChr)) {
+  mart_extract <- getBM(attributes = c("ensembl_gene_id", "mgi_symbol", "chromosome_name",
+                                       "start_position", "end_position"),
+                        filters = c("chromosome_name", "start", "end"),
+                        values = list(save_int_age$IntAgeChr[i], save_int_age$IntAgePos[i], save_int_age$IntAgePos[i]),
+                        mart = ensembl)
+  if(exists(c("gene", "gene_symbol"))) {
+    rm(gene, gene_symbol)
+  }
+  if (nrow(mart_extract) == 0 ) {
+    save_int_age$IntAgeGeneID[i] <- NA
+    save_int_age$IntAgeGeneSymbol[i] <- NA
+  } else if (nrow(mart_extract > 0)) {
+    for (x in 1:length(mart_extract$ensembl_gene_id)) {
+      if(exists("gene") == TRUE){
+        gene <- paste(gene, mart_extract$ensembl_gene_id[x], sep = ",")
+        gene_symbol <- paste(gene_symbol, mart_extract$mgi_symbol[x], sep = ",")
+      } else if (exists("gene") == FALSE) {
+        gene <- mart_extract$ensembl_gene_id[x]
+        gene_symbol <- mart_extract$mgi_symbol[x]
+      }
+    }
+    save_int_age$IntAgeGeneID[i] <- gene
+    save_int_age$IntAgeGeneSymbol[i] <- gene_symbol
+    rm(gene, gene_symbol)
+  }
+}
+# save annotated list for sharing
+write.csv(save_int_age, "./Threshold_eQTL_intAge.csv", row.names = FALSE, quote = FALSE)
 
 # Convert transcript and qtl position relative to chromosome positions
 # Convert to megabases
--- QTLmapping/pQTL_IntAge_map.R
+++ QTLmapping/pQTL_IntAge_map.R
@@ -1,5 +1,7 @@
 # R/3.4.1
 library(ggplot2)
+library(dplyr)
+library(biomaRt)
 setwd("/projects/korstanje-lab/ytakemon/JAC_DO_Kidney")
 pQTL_add <- read.csv("./QTLscan/output/pQTLAllAdditive.csv")
 pQTL_intAge <- read.csv("./QTLscan/output/pQTLAllInteractiveAge.csv")
@@ -30,6 +32,47 @@ LODthreshold_diff <- 7.5
 # Subset Int-Age LOD above total/full and diff
 Int_age <- pQTL_best[(pQTL_best$IntAgeLODDiff > LODthreshold_diff),] # above diff threshold
 Int_age <- Int_age[(Int_age$IntAgeLODFull > LODthreshold_total),] # above full/total threshold
+
+# Annotate Interactive-Age postion with genes and save file for sharing
+save_int_age <- Int_age[,c("id", "gene_id","symbol","chr","start","end", "biotype", "IntAgeChr","IntAgePos","IntAgeLODDiff","IntAgeLODFull")]
+save_int_age <- arrange(save_int_age, IntAgeChr, IntAgePos)
+# Annotate
+ensembl <- useEnsembl(biomart = "ENSEMBL_MART_ENSEMBL",
+                      dataset = "mmusculus_gene_ensembl",
+                      verbose = TRUE)
+
+# use listFilters(ensembl) for full list of filters
+save_int_age$IntAgeGeneID <- 0
+save_int_age$IntAgeGeneSymbol <- 0
+for (i in 1:length(save_int_age$IntAgeChr)) {
+  mart_extract <- getBM(attributes = c("ensembl_gene_id", "mgi_symbol", "chromosome_name",
+                                       "start_position", "end_position"),
+                        filters = c("chromosome_name", "start", "end"),
+                        values = list(save_int_age$IntAgeChr[i], save_int_age$IntAgePos[i], save_int_age$IntAgePos[i]),
+                        mart = ensembl)
+  if(exists(c("gene", "gene_symbol"))) {
+    rm(gene, gene_symbol)
+  }
+  if (nrow(mart_extract) == 0 ) {
+    save_int_age$IntAgeGeneID[i] <- NA
+    save_int_age$IntAgeGeneSymbol[i] <- NA
+  } else if (nrow(mart_extract > 0)) {
+    for (x in 1:length(mart_extract$ensembl_gene_id)) {
+      if(exists("gene") == TRUE){
+        gene <- paste(gene, mart_extract$ensembl_gene_id[x], sep = ",")
+        gene_symbol <- paste(gene_symbol, mart_extract$mgi_symbol[x], sep = ",")
+      } else if (exists("gene") == FALSE) {
+        gene <- mart_extract$ensembl_gene_id[x]
+        gene_symbol <- mart_extract$mgi_symbol[x]
+      }
+    }
+    save_int_age$IntAgeGeneID[i] <- gene
+    save_int_age$IntAgeGeneSymbol[i] <- gene_symbol
+    rm(gene, gene_symbol)
+  }
+}
+# save annotated list for sharing
+write.csv(save_int_age, "./Threshold_pQTL_intAge.csv", row.names = FALSE, quote = FALSE)
 
 # Convert transcript and qtl position relative to chromosome positions
 # Convert to megabases
