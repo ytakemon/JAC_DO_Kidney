7da7d410bb000ba0d1ced8e619f1ff3e0fd01874
ytakemon <yuka.takemon@gmail.com>
2017-10-02 10:17:10 -0400

    Slope calculation added to analysis

--- ANOVA/ANOVA_analysis.sh
+++ ANOVA/ANOVA_analysis.sh
@@ -5,7 +5,6 @@
 cd /projects/korstanje-lab/ytakemon/JAC_DO_Kidney/ANOVA
 # qsub -v I="input.Rdata output.csv",script=rscript_name
 qsub -v I="../../RNAseq_data/DO188b_kidney_noprobs.RData ../../Anova_output/kidney_anova_output.csv",script=anova_tests Rsubmit_args.sh
-
 # Calculate fdr/pval-BH for every p-value calculated from Petr's anova code
 # qsub -v I="anova_output.csv output.csv",script=rscript_name
 qsub -v I="kidney_anova_output.csv kidney_anova_fdr_output.csv",script=Kidney_ANOVA_fdr Rsubmit_args.sh
@@ -14,4 +13,5 @@ qsub -v I=kidney_anova_fdr_output.csv,script=Kidney_ANOVA_pval_hist Rsubmit_args
 # Identify significant genes if p-value and fdr are both <= 0.05
 qsub -v I="kidney_anova_fdr_output.csv kidney_anova_sig_output.csv",script=Kidney_ANOVA_significant Rsubmit_args.sh
 
-#
+# Modified ANOVA analysis to inlude slope of regression line.
+qsub -v I="../../RNAseq_data/DO188b_kidney_noprobs.RData ../../Anova_output/kidney_anova_slope_output.csv",script=anova_tests_slope Rsubmit_args.sh
