a73a35d1ccb6e7e314c8ff24006aef9725850c6e
ytakemon <yuka.takemon@gmail.com>
2017-10-09 07:52:05 -0400

    Rename files

--- ANOVA/ANOVA_analysis.sh
+++ ANOVA/ANOVA_analysis.sh
@@ -5,7 +5,7 @@
 # https://github.com/simecek/TheAgingProteome/blob/master/code/anova_tests.R
 cd /projects/korstanje-lab/ytakemon/JAC_DO_Kidney/ANOVA
 # qsub -v I="input.Rdata output.csv",script=rscript_name
-qsub -v I="../../RNAseq_data/DO188b_kidney_noprobs.RData ../../Anova_output/kidney_anova_output.csv",script=anova_tests Rsubmit_args.sh
+qsub -v I="../../RNAseq_data/DO188b_kidney_noprobs.RData ../../Anova_output/kidney_anova_output.csv",script=anova_tests_pairs Rsubmit_args.sh
 # Calculate fdr/pval-BH for every p-value calculated from Petr's anova code
 # qsub -v I="anova_output.csv output.csv",script=rscript_name
 qsub -v I="kidney_anova_output.csv kidney_anova_fdr_output.csv",script=Kidney_ANOVA_fdr Rsubmit_args.sh
@@ -15,23 +15,37 @@ qsub -v I=kidney_anova_fdr_output.csv,script=Kidney_ANOVA_pval_hist Rsubmit_args
 qsub -v I="kidney_anova_fdr_output.csv kidney_anova_sig_output.csv",script=Kidney_ANOVA_significant_pval_FDR Rsubmit_args.sh
 
 # Modified ANOVA analysis to inlude slope of regression line.
-qsub -v I="../../RNAseq_data/DO188b_kidney_noprobs.RData ../../Anova_output/kidney_anova_slope_output.csv",script=anova_tests_slope Rsubmit_args.sh
+qsub -v I="../../RNAseq_data/DO188b_kidney_noprobs.RData ../../Anova_output/kidney_anova_slope_output.csv",script=anova_tests_slope_pairs Rsubmit_args.sh
 # Plot slopes
 qsub -v I=kidney_anova_slope_output.csv,script=ANOVA_slope_plot Rsubmit_args.sh
 
-# FACTOR ANALYSIS DITCHED
+
+# Sharing gene lists with Gary & Ron -------------------------------------------
+cd /projects/korstanje-lab/ytakemon/JAC_DO_Kidney/ANOVA
+# qsub -v I="input.Rdata output.csv",script=rscript_name
+qsub -v I="../../RNAseq_data/DO188b_kidney_noprobs.RData ../../Anova_output/kidney_anova_output.csv",script=anova_tests_pairs Rsubmit_args.sh
+# Identify significant genes if p-value and fdr are both <= 0.05
+qsub -v I="kidney_anova_fdr_output.csv kidney_anova_sig_output.csv",script=Kidney_ANOVA_significant_pvalONLY Rsubmit_args.sh
+# Plot slopes, includes gene list output
+qsub -v I=kidney_anova_slope_output.csv,script=ANOVA_slope_plot Rsubmit_args.sh
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# FACTOR ANALYSIS DISCONTINUED
 # ANOVA: Changes in mRNA/Protein with Age as factor ----------------------------
 cd /projects/korstanje-lab/ytakemon/JAC_DO_Kidney/ANOVA
 # ANOVA mRNA/Protein with Age as factor. Generates p-values and slopes
 qsub -v I="../../RNAseq_data/DO188b_kidney_noprobs.RData ../../Anova_output/kidney_anova_output.csv",script=anova_tests_asfactor Rsubmit_args.sh
 # Plot pval histgrams
 qsub -v I=kidney_anova_factor_table.csv,script=Kidney_ANOVA_pval_hist_asfactor Rsubmit_args.sh
-
-# Push out list for Gary & Ron -------------------------------------------------
-cd /projects/korstanje-lab/ytakemon/JAC_DO_Kidney/ANOVA
-# qsub -v I="input.Rdata output.csv",script=rscript_name
-qsub -v I="../../RNAseq_data/DO188b_kidney_noprobs.RData ../../Anova_output/kidney_anova_output.csv",script=anova_tests Rsubmit_args.sh
-# Identify significant genes if p-value and fdr are both <= 0.05
-qsub -v I="kidney_anova_fdr_output.csv kidney_anova_sig_output.csv",script=Kidney_ANOVA_significant_pvalONLY Rsubmit_args.sh
-# Plot slopes, includes gene list output
-qsub -v I=kidney_anova_slope_output.csv,script=ANOVA_slope_plot Rsubmit_args.sh
