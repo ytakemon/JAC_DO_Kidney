0dc16c242102c8bcd65fb638cbfa9e5c5fa90a04
ytakemon <yuka.takemon@gmail.com>
2017-10-31 12:45:10 -0400

    fix list, gene name converted to dates!

---
+++ QTLmapping/eQTLint_Akt1Add.R
@@ -0,0 +1,56 @@
+# run with:
+# qsub -v script=eQTLint_Akt1Add Rsubmit_args.sh
+
+library(qtl2geno)
+library(qtl2scan)
+library(qtl2convert)
+setwd("/projects/korstanje-lab/ytakemon/JAC_DO_Kidney")
+load("./shiny_annotation.RData")
+load("./RNAseq_data/DO188b_kidney.RData")
+
+# Get list of genes with trans eQTL
+list <- read.csv("./QTLscan/output/Threshold8_eQTL_intAge.csv", header = TRUE, stringsAsFactors = FALSE)
+list <- list[list$IntAgeChr == 12, ]
+list <- list$symbol
+
+# Identify gene name
+trans <- "Akt1"
+other.ids <- function(gene.name, level) {
+  if (level == "mRNA") {
+    sel <- which(mRNA.list$symbol == gene.name)[1]
+    if (!is.na(sel)) return(mRNA.list[sel,]) else return(c(NA,NA,NA))
+  }
+  if (level == "protein") {
+    sel <- which(protein.list$symbol == gene.name)[1]
+    if (!is.na(sel)) return(protein.list[sel,]) else return(c(NA,NA,NA))
+  }
+}
+trans <- other.ids(trans, "mRNA")
+
+# prepare data for qtl2
+probs <- probs_doqtl_to_qtl2(genoprobs, snps, pos_column = "bp")
+snps$chr <- as.character(snps$chr)
+snps$chr[snps$chr=="X"] <- "20"
+map <- map_df_to_list(map = snps, pos_column = "bp")
+
+for (p in 1:length(list)){
+
+  cat("Scanning ",p," out of ",length(list),"\n")
+  addcovar <- model.matrix(~ Sex + Age + Generation + expr.mrna[,trans$id], data=annot.samples)
+  intcovar <- model.matrix(~ Sex, data=annot.samples)
+
+  p <- list[p]
+  p <- other.ids(p, "mRNA")
+
+  # Lod score
+  lod <- scan1(genoprobs=probs,
+               kinship=Glist,
+               pheno=expr.mrna[, p$id],
+               addcovar=addcovar[,-1],
+               intcovar=intcovar[,-1],
+               cores=10, reml=TRUE)
+
+  # Save lod object
+  file_name <- paste0("./QTLscan/intscan_mrna_addAkt1/", p$id, "_", p$symbol, ".rds")
+  saveRDS(lod, file = file_name)
+}
