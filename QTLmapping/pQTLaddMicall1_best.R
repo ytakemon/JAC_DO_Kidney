dc98099c5c48b78a45370ec2c393ddb3f019b854
ytakemon <yuka.takemon@gmail.com>
2017-10-31 14:57:03 -0400

    Initial commit

---
+++ QTLmapping/pQTLaddMicall1_best.R
@@ -0,0 +1,54 @@
+# R/3.4.1
+setwd("/projects/korstanje-lab/ytakemon/JAC_DO_Kidney/")
+load("./RNAseq_data/DO188b_kidney.RData")
+library(dplyr)
+
+# Get list of genes with trans eQTL
+list <- read.csv("./QTLscan/output/Threshold8_pQTL_intAge.csv", header = TRUE, stringsAsFactors = FALSE)
+list <- list[list$IntAgeChr == 15, ]
+
+# parameters
+addscan.dir <- "./QTLscan/addscan_prot_Micall1/"
+intscan.dir.Age <- "./QTLscan/intscan_prot/Age/"
+file.name <- function(i) paste0(annot.protein$id[i],"_",annot.protein$symbol[i],".rds") # should be protein id
+output.file1 <- "./QTLscan/output/pQTLBestperGeneAddMicall1.csv"
+
+annot.protein <- annot.protein[annot.protein$id %in% list$id,]
+output <- annot.protein[,c(1:5,9)]
+output$AdditiveLOD <- output$AdditivePos <-  output$AdditiveChr <- NA
+output$IntAgeLODFull <- output$IntAgeLODDiff <- output$IntAgePos <- output$IntAgeChr <- NA
+
+# for
+for (i in 1:nrow(output)) {
+  if (i %% 10 == 0) print(i)
+  fit0 <- readRDS(paste0(addscan.dir,file.name(i)))
+  fitAge <- readRDS(paste0(intscan.dir.Age, file.name(i)))
+
+  # additive scan
+  dt <- data.frame(AdditiveChr=sapply(strsplit(rownames(fit0),"_"), "[", 1),
+                   AdditivePos=as.numeric(sapply(strsplit(rownames(fit0),"_"), "[", 2)),
+                   AdditiveLOD=fit0[,1], stringsAsFactors=FALSE)
+  dt2 <- dt %>% group_by(AdditiveChr) %>%
+         summarize(AdditivePos = AdditivePos[which.max(AdditiveLOD)[1]],
+                   AdditiveLOD = max(AdditiveLOD)) %>%
+         arrange(-AdditiveLOD)
+
+  output[i, c("AdditiveChr", "AdditivePos", "AdditiveLOD")] <- dt2[1,]
+
+  # int. scan - Age
+  stopifnot(rownames(fit0) == rownames(fitAge))
+  dt <- data.frame(IntAgeChr=sapply(strsplit(rownames(fit0),"_"), "[", 1),
+                   IntAgePos=as.numeric(sapply(strsplit(rownames(fit0),"_"), "[", 2)),
+                   IntAgeLODDiff=fitAge[,1] - fit0[,1],
+                   IntAgeLODFull = fitAge[,1],
+                   stringsAsFactors=FALSE)
+  dt2 <- dt %>% group_by(IntAgeChr) %>%
+         summarize(IntAgePos = IntAgePos[which.max(IntAgeLODFull)[1]],
+                   IntAgeLODDiff=IntAgeLODDiff[which.max(IntAgeLODFull)[1]],
+                   IntAgeLODFull = max(IntAgeLODFull)) %>%
+         arrange(-IntAgeLODDiff)
+  output[i, c("IntAgeChr", "IntAgePos", "IntAgeLODDiff", "IntAgeLODFull")] <- dt2[1,]
+}
+
+# collect rows into one data frame
+write.csv(output, file=output.file1, row.names=FALSE)
