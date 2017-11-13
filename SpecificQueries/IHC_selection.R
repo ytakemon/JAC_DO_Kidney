0d50e16afc6fec7068ad1cdc4e9c30701cc6feb6
ytakemon <yuka.takemon@gmail.com>
2017-11-13 14:03:46 -0500

    Works for now, but very cluncky

--- SpecificQueries/IHC_selection.R
+++ SpecificQueries/IHC_selection.R
@@ -1,4 +1,4 @@
-library(ggplot2)
+# Identify animals that have the average given expression values for the given group.
 library(dplyr)
 setwd("/projects/korstanje-lab/ytakemon/JAC_DO_Kidney")
 load("./RNAseq_data/DO188b_kidney_noprobs.RData")
@@ -6,10 +6,10 @@ load("./shiny_annotation.RData")
 
 # Selection quality:
 Gene_name <- "Lrp2"
-Data <- "mRNA"
+Data <- "protein"
 Sex <- "M"
 Age <- c(6, 18)
-Num <- 3 # Number of animals to select
+Num <- 3
 
 # Identify gene name
 other.ids <- function(gene.name, level) {
@@ -24,8 +24,56 @@ other.ids <- function(gene.name, level) {
 }
 gene <- other.ids(Gene_name, Data)
 
+# Get expression data
 if(Data == "mRNA"){
   annot.samples$expr <- expr.mrna[, gene$id]
 } else if (Data == "protein"){
   annot.samples$expr <- expr.protein[, gene$protein_id]
 }
+
+# Subset selction
+samples <- annot.samples[ annot.samples$Age %in% Age,]
+samples <- samples[ samples$Sex %in% Sex,]
+cal <- aggregate(expr ~ Age, data = samples, mean)
+mo6 <- samples[ samples$Age == 6,] %>% arrange(expr)
+mo18 <- samples[ samples$Age == 18,] %>% arrange(expr)
+
+# identify mean
+min6 <- which.min(abs(mo6$expr - cal[cal$Age == 6,]$expr))
+min18 <- which.min(abs(mo18$expr - cal[cal$Age == 18,]$expr))
+
+if( Num == 1){
+  Num <- 0
+  min6 <- min6 + Num
+  min18 <- min18 + Num
+} else if( Num == 2){
+  Num <- c(0,1)
+  min6 <- min6 + Num
+  min18 <- min18 + Num
+} else if( Num == 3){
+  Num <- c(-1, 0, 1)
+  min6 <- min6 + Num
+  min18 <- min18 + Num
+} else if( Num == 4){
+  Num <- c(-1, 0, 1, 2)
+  min6 <- min6 + Num
+  min18 <- min18 + Num
+} else if( Num == 5){
+  Num <- c(-2, -1, 0, 1, 2)
+  min6 <- min6 + Num
+  min18 <- min18 + Num
+} else if( Num == 6){
+  Num <- c(-2, -1, 0, 1, 2, 3)
+  min6 <- min6 + Num
+  min18 <- min18 + Num
+} else if( Num == 7){
+  Num <- c(-3, -2, -1, 0, 1, 2, 3)
+  min6 <- min6 + Num
+  min18 <- min18 + Num
+}
+min6 <- mo6[min6,]
+min18 <- mo18[min18,]
+select <- rbind(min6, min18)
+
+# Print results
+print(select)
