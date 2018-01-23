ce26c994112d9f8c5c9a3dc9608e7fdc1e266c73
ytakemon <yuka.takemon@gmail.com>
2018-01-23 13:46:37 -0500

    relocate

--- QTL_Permute/eQTL/Napsa_mRNA_permute.R
+++ QTL_Permute/eQTL/Napsa_mRNA_permute.R
@@ -1,11 +1,11 @@
-#qsub -v script=Sf3a1_mRNA_permute Rsubmit_args.sh
+#qsub -v script=Napsa_mRNA_permute Rsubmit_args.sh
 library(qtl2)
 library(qtl2convert)
 library(dplyr)
 setwd("/projects/korstanje-lab/ytakemon/JAC_DO_Kidney")
 load("./RNAseq_data/DO188b_kidney.RData")
 load("./shiny_annotation.RData")
-name <- "Sf3a1"
+name <- "Napsa"
 
 other.ids <- function(gene.name, level) {
     sel <- which(mRNA.list$symbol == gene.name)[1]
@@ -16,7 +16,7 @@ id <- other.ids(name)
 
 # prepare data for qtl2
 probs <- probs_doqtl_to_qtl2(genoprobs, snps, pos_column = "pos")
-K <- calc_kinship(probs, "loco", cores=3)
+K <- calc_kinship(probs, "loco", cores=10)
 snps$chr <- as.character(snps$chr)
 map <- map_df_to_list(map = snps, pos_column = "pos")
 
@@ -33,4 +33,4 @@ addperm <- scan1perm(genoprobs = probs,
 
 # save perms
 file_name <- paste0("./QTLperm/eQTL_", name, "_1000perm.rds")
-saveRDS(perm, file_name)
+saveRDS(addperm, file_name)
