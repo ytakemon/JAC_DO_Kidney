cb13543ef47f7e672841ac55538ba48170a9e5ac
ytakemon <yuka.takemon@gmail.com>
2018-04-24 12:44:59 -0400

    updated

--- QTLmapping/scanBestChr/Gather_scanBestChr_all_m.R
+++ QTLmapping/scanBestChr/Gather_scanBestChr_all_m.R
@@ -7,8 +7,8 @@ options(dplyr.width = Inf)
 library(tidyverse)
 
 # parameters
-diffscan_dir <- "./QTLscan/scanBestChr_mrna/intmaxchr"
-IntAge_output_file <- "./QTLscan/scanBestChr_mrna/BestChr_BestperGene_mrna.csv"
+diffscan_dir <- "./QTLscan/scanBestMarker_mrna/maxLODscan"
+IntAge_output_file <- "./QTLscan/scanBestMarker_mrna/BestMarker_BestperGene_mrna.csv"
 
 gatherdf <- function(dir, pattern){
   file_list <- list.files(dir,pattern,full.names=TRUE)
@@ -29,4 +29,3 @@ diffLOD <- gatherdf(dir = diffscan_dir, pattern = "maxLODscan_batch_")
 
 # write table
 write_csv(diffLOD, IntAge_output_file)
- 
